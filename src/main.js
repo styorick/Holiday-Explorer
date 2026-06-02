8d3e1563f2ec6218c6fed9a31e64eed6ae37840d
styorick <steenhoudt.yorick@gmail.com>
2026-06-02 17:15:34 +0200

    testen van API's
    testen hoe de de api's tevoorschijn komen in de console log

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import './style.css'
+'use strict';
+
+fetch('https://date.nager.at/api/v3/publicholidays/2026/NL')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Er ging iets mis:', error));
+
+fetch('https://restcountries.com/v3.1/all?fields=name,cca2,flags')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Er ging iets mis:', error));
